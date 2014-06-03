20a81e1bdd80bcb8fae0c49bf407564d0d0a4bf8
Matthew Wampler-Doty <matt@w-d.org>
2014-06-02 20:39:12 -0500

    Adding POST

--- curl_script.sh
+++ curl_script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-echo "http://localhost:3000/average-of-averages =>" \
+echo "GET http://localhost:3000/average-of-averages =>" \
 	$(curl -s "http://localhost:3000/average-of-averages")
+
+echo "POST http://localhost:3000/add-key?key=foo&value=9 =>" \
+	$(curl -s "http://localhost:3000/add-key" -d key=foo -d value=9)
