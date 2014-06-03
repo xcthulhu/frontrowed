4cc846240cbd215ac1fb7cf256a129c9b12b131f
Matthew Wampler-Doty <matt@w-d.org>
2014-06-02 20:45:27 -0500

    Adding key-average call, and another add-key call

--- curl_script.sh
+++ curl_script.sh
@@ -5,3 +5,9 @@ echo "GET http://localhost:3000/average-of-averages =>" \
 
 echo "POST http://localhost:3000/add-key?key=foo&value=9 =>" \
 	$(curl -s "http://localhost:3000/add-key" -d key=foo -d value=9)
+
+echo "POST http://localhost:3000/add-key?key=bar&value=7 =>" \
+	$(curl -s "http://localhost:3000/add-key" -d key=bar -d value=7)
+
+echo "GET http://localhost:3000/key-average?key=foo =>" \
+	$(curl -s "http://localhost:3000/key-average" -G -d key=foo)
